a61eecb606b87c73c3a34d5d0f2c2e1776ec8b4d
ctarleii <egorcerencov0023@gmail.com>
2023-07-17 21:12:20 +1000

    second commit

--- drf/books/views.py
+++ drf/books/views.py
@@ -1,54 +1,31 @@
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import viewsets
 from django.forms import model_to_dict
 
 from books.models import Books
 from books.serializers import BooksSerializer
 
 
-class BooksAPIList(generics.ListCreateAPIView):
+class BooksViewSet(viewsets.ModelViewSet):
     queryset = Books.objects.all()
     serializer_class = BooksSerializer
 
 
-class BooksApiView(APIView):
-    def get(self, request):
-        w = Books.objects.all()
-        return Response({'posts': BooksSerializer(w, many=True).data})
+# class BooksAPIList(generics.ListCreateAPIView):
+#     queryset = Books.objects.all()
+#     serializer_class = BooksSerializer
+#
+#
+# class BooksAPIUpdate(generics.UpdateAPIView):
+#     queryset = Books.objects.all()
+#     serializer_class = BooksSerializer
+#
+#
+# class BooksAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Books.objects.all()
+#     serializer_class = BooksSerializer
 
-    def post(self, request):
-        serializer = BooksSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
 
-        return Response({'post': serializer.data})
 
-    def put(self, request, *args, **kwargs):
-        pk = kwargs.get('pk', None)
-        print(f'put {kwargs}')
-        if not pk:
-            return Response({'error': 'Method PUT not allowed'})
-
-        try:
-            instance = Books.objects.get(pk=pk)
-        except:
-            return Response({'error': "Object doesn't not exists"})
-
-        serializer = BooksSerializer(data=request.data, instance=instance)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-        return Response({'post': serializer.data})
-
-    def delete(self, request, *args, **kwargs):
-        pk = kwargs.get("pk", None)
-        if not pk:
-            return Response({"error": "Method DELETE not allowed"})
-
-        try:
-            Books.objects.get(id=pk).delete()
-        except:
-            return Response({'error': "Object doesn't not exists"})
-
-        return Response({'post': f'delete post {pk}'})
--- drf/drf/urls.py
+++ drf/drf/urls.py
@@ -15,13 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from books.views import BooksApiView, BooksAPIList
+from books.views import *
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register(r'books', BooksViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/books-list/', BooksAPIList.as_view()),
-    path('api/v1/books-list/<int:pk>/', BooksAPIList.as_view()),
+    path('api/v1/', include(router.urls)),
+    # path('api/v1/books-list/', BooksViewSet.as_view({'get': 'list'})),
+    # path('api/v1/books-list/<int:pk>/', BooksViewSet.as_view({'put': 'update'})),
 ]
 
